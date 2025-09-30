38997673e9207dde6f0da81ef3f15a64a6ba81a2
sez27 <sez6@njit.edu>
2025-09-30 15:51:49 -0400

    Changed landing page background, applied tailwind css

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,58 +1,37 @@
-import { ImageBackground, Text, View, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity, Image } from "react-native";
 import { Link } from "expo-router";
+import { NativeWindStyleSheet } from "nativewind";
 
-export default function LandingScreen() {
+NativeWindStyleSheet.setOutput({ default: "native" });
+
+export default function Page() {
   return (
-    <ImageBackground
-      source={require('./assets/images/landingPage.png')}
-      className="flex-1 justify-center items-center bg-base-300"
-      resizeMode="cover"
-    >
-      <View className="p-5 flex-1 justify-center items-center w-full">
-
-        <View className="mb-10 items-center">
-
-          <Text
-            className="text-4xl text-olive-500 font-semibold"
-            style={{
-              textShadowColor: 'rgba(255, 255, 255, 0.8)',
-              textShadowOffset: { width: 1, height: 1 },
-              textShadowRadius: 2,
-            }}
-          >
-            Find a Chef
-          </Text>
-
-          <Text
-            className="text-3xl text-olive-400 font-bold"
-            style={{
-              textShadowColor: 'rgba(255, 255, 255, 0.8)',
-              textShadowOffset: { width: 1, height: 1 },
-              textShadowRadius: 2,
-            }}
-          >
-            ChefAsap
-          </Text>
+    <View className="flex-1 justify-center items-center bg-base-100">
+      <View className="p-6 rounded-2xl bg-olive-200 items-center">
+        <View className="mb-8 items-center">
+          {/* <Image
+            source={require('../assets/images/chefAsapLogo.png')}
+            className="w-16 h-16 mb-4"
+            resizeMode="contain"
+          /> */}
+          <Text className="text-xl font-bold text-olive-400">Find a Chef</Text>
+          <Text className="text-3xl font-bold text-olive-500">ChefAsap</Text>
         </View>
 
-        <Link href="/SignUpScreen" asChild>
-          <TouchableOpacity
-            className="py-4 px-8 rounded-full items-center bg-olive-400 border-2 border-olive-100 shadow-lg"
-          >
-            <Text className="text-lg font-bold text-olive-100">Sign Up</Text>
+        <Link href="/signup" asChild>
+          <TouchableOpacity className="bg-olive-300 py-3 px-8 rounded-lg mb-2">
+            <Text className="text-base-300 font-bold text-lg">Sign Up</Text>
           </TouchableOpacity>
         </Link>
 
-        <View className="h-2.5" />
+        <View className="h-2" />
 
-        <Link href="/SignInScreen" asChild>
-          <TouchableOpacity
-            className="py-4 px-8 rounded-full items-center bg-olive-100 border-2 border-olive-400 shadow-lg"
-          >
-            <Text className="text-lg font-bold text-olive-400">Log In</Text>
+        <Link href="/signin" asChild>
+          <TouchableOpacity className="bg-olive-300 py-3 px-8 rounded-lg">
+            <Text className="text-base-300 font-bold text-lg">Log In</Text>
           </TouchableOpacity>
         </Link>
       </View>
-    </ImageBackground>
+    </View>
   );
 }
